80c607a3d71ccdc3a489e6147b0028ffd06922b6
CrayzFrog-tech <allanmohamad7@gmail.com>
2022-03-30 18:47:58 +0200

    day 2 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
- import HomeScreen from './HomeScreen';
+import HomeScreen from './HomeScreen';
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen/>
-      
+      <HomeScreen />
+
     </div>
   );
 }
--- src/Banner.js
+++ src/Banner.js
@@ -7,11 +7,11 @@ function Banner() {
 
     const [movie, setMovie] = useState([]);
     useEffect(() => {
-        async function fetchData(){
+        async function fetchData() {
             const request = await axios.get(requests.fetchNetflixOriginals);
             setMovie(
                 request.data.results[
-                    Math.floor(Math.random() * request.data.results.length - 1)
+                Math.floor(Math.random() * request.data.results.length - 1)
                 ]
             );
             return request;
@@ -21,7 +21,7 @@ function Banner() {
     }, []);
     console.log(movie);
     function truncate(string, n) {
-        return string?.length > n ? string.substr(0, n -1) + '...' : string;
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string;
 
     }
     return (
@@ -31,12 +31,12 @@ function Banner() {
             backgroundPosition: "center center",
         }}>
             <div className='banner__contents'>
-                <h1 className='banner__title'>Movie Name</h1>
+                <h1 className='banner__title'>{movie?.title || movie?.name || movie?.orignal_name}</h1>
                 <div className='banner__buttons'>
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>My List</button>
                 </div>
-                <h1 className='banner__description'>{truncate(`This is a test description`, 150)}</h1>
+                <h1 className='banner__description'>{truncate(movie?.overview, 150)}</h1>
             </div>
 
             <div className='banner--fadeBottom'></div>
