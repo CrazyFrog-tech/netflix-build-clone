a2e88235faedac48b23e208cac24a7dee5dadddb
Mohamad <Mohamad.Allan@macaw.nl>
2022-04-08 19:49:05 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {
@@ -7,16 +7,39 @@ import {
   Route,
 } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => { 
+      if(userAuth){
+        console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }));
+
+      }else{
+        dispatch(logout);
+
+      }
+    });
+    return unsubscribe;
+  },[]); 
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<HomeScreen></HomeScreen>}>
-          </Route>
-          <Route path='/login' element={<LoginScreen></LoginScreen>}>
-          </Route>
+          {user ? <Route path='/' element={<HomeScreen></HomeScreen>}>
+          </Route> : <Route path='/' element={<LoginScreen></LoginScreen>}>
+          </Route>}
+          
+          {/* <Route path='/login' element={<LoginScreen></LoginScreen>}>
+          </Route> */}
         </Routes>
       </Router>
 
@@ -27,3 +50,4 @@ function App() {
 }
 
 export default App;
+ 
